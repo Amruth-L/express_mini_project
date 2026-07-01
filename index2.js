01d27ec8bb978faa195959fdde48741a0b60a6aa
Amruth-L <amruthln21@gmail.com>
2026-07-01 22:53:30 +0530

    health

---
+++ index2.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const users = [{
+    name: "john",
+    kidneys: [{
+        healthy: false
+    }]
+}]
+app.use(express.json());
+app.get("/", function (req, res) {
+    const johnkidneys = users[0].kidneys;
+    const numberofkidneys = johnkidneys.length;
+    let numberofhealthykidneys = 0;
+    for (let i = 0; i < johnkidneys.length; i++) {
+        if (johnkidneys[i].healthy) {
+            numberofhealthykidneys = numberofhealthykidneys + 1;
+
+        }
+    }
+    const numberofunhealthykidneys = numberofkidneys - numberofhealthykidneys;
+    res.json({
+        numberofkidneys,
+        numberofhealthykidneys,
+        numberofunhealthykidneys
+    })
+})
+
+app.post("/", function (req, res) {
+
+    const ishealthy = req.body.ishealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
+
+app.listen(3003);
+app.put("/", function (req, res) {
+
+
+})
+app.delete("/", function (req, res) {
+
+
+})
