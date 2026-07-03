0f8fc438151ca13032f5fba1cb31b512a8e296a2
Amruth-L <amruthln21@gmail.com>
2026-07-03 19:22:48 +0530

    middleware

--- index4.js
+++ index4.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const app = express();
+//function that returns the boolean (if the age of person is more than 14)
+function isOldEnough(age) {
+    if (age >= 14) {
+        return true;
+    } else {
+        return false;
+    }
+}
 app.get("/ride1", function (req, res) {
-    res.json({
-        msg: "you have successfully riden the ride 1"
-    })
+    console.log(req.query);
+    if (isOldEnough(req.query.age)) {
+        res.json({
+            msg: "you have successfully riden the ride 1"
+        })
+    } else {
+        res.status(411).json({
+            msg: "Sorry you are not at an age yet"
+        })
+    }
 
 })
 app.listen(3006);
